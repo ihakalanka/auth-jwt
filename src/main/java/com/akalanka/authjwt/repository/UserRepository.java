13f888ee54667fb0652b1e9db26b049d5849ff2c
ihakalanka <akalankaih19@gmail.com>
2023-01-30 17:15:31 +0530

    created user repository

---
+++ src/main/java/com/akalanka/authjwt/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.akalanka.authjwt.repository;
+
+import com.akalanka.authjwt.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
