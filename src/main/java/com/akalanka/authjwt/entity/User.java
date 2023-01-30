e1c56cd9341b021aea3b3cb263420d34173bd073
ihakalanka <akalankaih19@gmail.com>
2023-01-30 17:14:20 +0530

    created user entity

---
+++ src/main/java/com/akalanka/authjwt/entity/User.java
@@ -0,0 +1,24 @@
+package com.akalanka.authjwt.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+}
