15edfd9e988625b446ea2132c5f4f0e7c310d2fb
ihakalanka <akalankaih19@gmail.com>
2023-01-31 21:00:36 +0530

    updated security configuration file

---
+++ src/main/java/com/akalanka/authjwt/entity/JwtRequest.java
@@ -0,0 +1,10 @@
+package com.akalanka.authjwt.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
--- src/main/java/com/akalanka/authjwt/entity/User.java
+++ src/main/java/com/akalanka/authjwt/entity/User.java
@@ -19,8 +19,6 @@ public class User {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
-    @Column(name = "user_name", unique = true)
-    private String userName;
     @Column(name = "email", unique = true)
     private String email;
     @Column(name = "password")
