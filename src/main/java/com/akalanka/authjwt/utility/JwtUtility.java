6bcc9c526cbf95ca8e21b0789e94b518896b116b
ihakalanka <akalankaih19@gmail.com>
2023-01-31 15:07:44 +0530

    created getAll claims from string method

--- src/main/java/com/akalanka/authjwt/utility/JwtUtility.java
+++ src/main/java/com/akalanka/authjwt/utility/JwtUtility.java
@@ -8,5 +8,11 @@ import java.io.Serializable;
 
 @Component
 public class JwtUtility implements Serializable {
+    private static final long serialVersionUID = -2550185165626007488L;
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+    private String secret = "secret";
 
+    public Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
 }
