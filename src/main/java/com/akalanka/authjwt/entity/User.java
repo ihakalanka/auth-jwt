24857afb8e32fd049d50a4785adce7c34d3c7878
ihakalanka <akalankaih19@gmail.com>
2023-01-31 16:29:16 +0530

    created another field inside the user class

--- src/main/java/com/akalanka/authjwt/entity/User.java
+++ src/main/java/com/akalanka/authjwt/entity/User.java
@@ -19,6 +19,8 @@ public class User {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
+    @Column(name = "user_name", unique = true)
+    private String userName;
     @Column(name = "email", unique = true)
     private String email;
     @Column(name = "password")
