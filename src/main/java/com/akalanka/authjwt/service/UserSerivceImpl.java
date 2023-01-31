dedc9e2354fe3db6e9545410a4fa34e8714c600d
ihakalanka <akalankaih19@gmail.com>
2023-01-31 15:06:56 +0530

    created jwt utility class

--- src/main/java/com/akalanka/authjwt/service/UserSerivceImpl.java
+++ src/main/java/com/akalanka/authjwt/service/UserSerivceImpl.java
@@ -3,6 +3,7 @@ package com.akalanka.authjwt.service;
 import com.akalanka.authjwt.entity.User;
 import com.akalanka.authjwt.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +11,13 @@ public class UserSerivceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public User save(User user) {
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
         return userRepository.save(user);
     }
 
---
+++ src/main/java/com/akalanka/authjwt/utility/JwtUtility.java
@@ -0,0 +1,12 @@
+package com.akalanka.authjwt.utility;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+public class JwtUtility implements Serializable {
+
+}
